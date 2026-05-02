5d3b053c1ae97b5a0a70bd6897d0d8075ba9f81b
Paulo Henrique <pauloramosh26@gmail.com>
2026-05-02 18:56:29 -0300

    teste

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
-import Formulario from "./components/formulario/Formulario";
+import Formulario from "./components/Formulario/Formulario-temp";
 
 function App() {
   return (
