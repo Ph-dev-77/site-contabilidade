ef428c1f8669a07bc80f42f224d901dfe734ed06
Paulo Henrique <pauloramosh26@gmail.com>
2026-05-02 10:52:52 -0300

    Testando compilação no vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './components/Header/Header';
-import Hero from './components/Hero/Hero';
-import Formulario from './components/Formulario/Formulario';
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import Formulario from "./components/formulario/Formulario";
 
 function App() {
   return (
